2ebef6b1910dfef505e1869a6516e8722afb9f46
Linssen21 <vhynlinssen@gmail.com>
2020-09-21 23:43:14 +0800

    redis update

--- app/services/cache.js
+++ app/services/cache.js
@@ -6,6 +6,7 @@ if (process.env.NODE_ENV === "production") {
   client = redis.createClient({
     url: process.env.REDIS_URL,
     password: process.env.REDIS_PASSWORD,
+    port: process.env.REDIS_PORT,
   });
 } else {
   client = redis.createClient();
