5ef4d0026afa9ba4ed4a943c2310d1cc6bacbe8e
Linssen21 <vhynlinssen@gmail.com>
2020-09-23 10:47:59 +0800

    security

--- app/index.js
+++ app/index.js
@@ -4,6 +4,12 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const path = require("path");
 
+// Security
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const compression = require("compression");
+const xss = require("xss-clean");
+
 const AppError = require("./utils/appError");
 
 const accountRouter = require("./routes/authRoutes");
@@ -22,35 +28,30 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+// Set security HTTP HEADERS
+app.use(helmet());
 /**
- * @description - Catch not found routes
- * const err = new Error(`Can't find ${req.originalUrl} on this server);
- * err.status = 'fail';
- * err.statusCode = 404;
- * next(err);
+ * Protetion agains DDOS and Brute force attack
+ * 200 request from the same IP in 1 hour
  */
-// app.get("/api/error", (req, res, next) => {
-//   next(
-//     new AppError("Invalid input", 401, [
-//       {
-//         field: "password",
-//         message: "Password require's 8 Character",
-//       },
-//       {
-//         field: "username",
-//         message: "Username already exists",
-//       },
-//     ])
-//   );
-// });
+const limiter = rateLimit({
+  max: 200,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many request from this IP, please try again in an hour",
+});
+// affects all route with the URL starting with /api
+app.use("/api", limiter);
+app.use(xss());
+app.use(compression());
 
+app.use(compression());
 app.use("/api/v1/account", accountRouter);
 app.use("/api/v1/detachment", detachmentRouter);
 app.use("/api/v1/employee", employeeRouter);
 
 // Load Frontend
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "development") {
+  app.use(express.static(path.join(__dirname, "..", "client", "build")));
   app.get("*", (req, res) => {
     res.sendFile(
       path.resolve(__dirname, "..", "client", "build", "index.html")
