471e6448c698e1846eb561e397dfceef342f362e
Linssen21 <vhynlinssen@gmail.com>
2020-09-23 11:26:11 +0800

    security

--- app/index.js
+++ app/index.js
@@ -50,7 +50,7 @@ app.use("/api/v1/detachment", detachmentRouter);
 app.use("/api/v1/employee", employeeRouter);
 
 // Load Frontend
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "..", "client", "build")));
   app.get("*", (req, res) => {
     res.sendFile(
